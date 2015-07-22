ea6996567ac5f920b32feecd89d0948f9eee4dd8
Audrey Carlsen <acarlsen@seattletimes.com>
2015-07-21 17:12:29 -0700

    it's alive!

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,10 @@
 require("component-responsive-frame/child");
 var d3 = require("d3");
 
-var diameter = 620,
+var diameter = 500,
     dropdown = document.querySelector("select");
 
-var svg = d3.select("responsive-child").append('svg')
+var svg = d3.select(".bubbles").append('svg')
   .attr('width', diameter)
   .attr('height', diameter)
 
@@ -18,12 +18,10 @@ var bubble = d3.layout.pack()
     .padding(1.5)
     .value(d => d.size);
 
-var drawBubbles = function() {
+var drawBubbles = function(selectedYear) {
   var duration = 1000;
 
-  var selectedYear = dropdown.value;
   var yearData = namesData.filter(function(name) { return name.year == selectedYear && name.total_perc < 10 });
-
 
   var nodes = bubble.nodes({children: yearData})
     .filter(d => !d.children); // filter out the outer bubble
@@ -34,19 +32,18 @@ var drawBubbles = function() {
   var entering = node.enter()
     .append('g')
     .attr('transform', d => `translate(${d.x}, ${d.y})`)
-    .attr('class', 'node')
+    .attr('class', 'node');
     
   entering.append("circle")
     .attr("r", d => 0)
     .style('opacity', 1)
     .style("fill", function(d) {
-      console.log("fill", d);
       if (d.sex == "F") { 
         return "#fcc79b";
       } else {
         return "#c1ceaf";
       }
-    })
+    });
   
   entering.append("text")
     .style("opacity", 0)
@@ -59,25 +56,37 @@ var drawBubbles = function() {
     .duration(duration)
     .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
     
-  transition.select("circle")
-    .attr("r", d => d.r);
+  transition.select("circle").attr("r", d => d.r);
 
-  transition.select("text")
-    .style("opacity", 1)
+  transition.select("text").style("opacity", 1);
   
   var exiting = node.exit()
     .transition()
-    .duration(duration)
+    .duration(duration);
   
-  exiting.select("circle")
-    .attr("r", d => 0)
+  exiting.select("circle").attr("r", d => 0);
 
-  exiting.select("text")
-    .style("opacity", 0)
+  exiting.select("text").style("opacity", 0);
   
-  exiting.remove()
+  exiting.remove();
 }
 
-drawBubbles();
-document.addEventListener("change", function() { drawBubbles() });
+dropdown.addEventListener("change", function() { 
+  clearTimeout(loop);
+  drawBubbles(dropdown.value);
+});
+
+var years = [1980, 1985, 1990, 1995, 2000, 2005, 2010, 2013];
+var i = 0;
+
+var loop = null;
+var tick = function() {
+  dropdown.value = years[i];
+  drawBubbles(dropdown.value);
+  i = (i + 1) % years.length;
+  loop = setTimeout(tick, i == 0 ? 3000 : 1500);
+};
+
+tick();
+
 
